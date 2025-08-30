3adcd098031a0c53fcc54adb57997a458b3604f8
Justin Chen <justinchencya@Justins-MacBook-Pro.local>
2025-08-30 13:23:10 -0700

    Sync local build files

---
+++ build/EchoSnap.build/Debug-iphonesimulator/EchoSnap.build/DerivedSources/GeneratedAssetSymbols.h
@@ -0,0 +1,21 @@
+#import <Foundation/Foundation.h>
+
+#if __has_attribute(swift_private)
+#define AC_SWIFT_PRIVATE __attribute__((swift_private))
+#else
+#define AC_SWIFT_PRIVATE
+#endif
+
+/// The "button-accept" asset catalog image resource.
+static NSString * const ACImageNameButtonAccept AC_SWIFT_PRIVATE = @"button-accept";
+
+/// The "button-close" asset catalog image resource.
+static NSString * const ACImageNameButtonClose AC_SWIFT_PRIVATE = @"button-close";
+
+/// The "button-reset" asset catalog image resource.
+static NSString * const ACImageNameButtonReset AC_SWIFT_PRIVATE = @"button-reset";
+
+/// The "logo-info-page" asset catalog image resource.
+static NSString * const ACImageNameLogoInfoPage AC_SWIFT_PRIVATE = @"logo-info-page";
+
+#undef AC_SWIFT_PRIVATE
